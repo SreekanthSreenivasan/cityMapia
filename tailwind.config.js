40224f0d1222acf58399f7789f59b46ab283d482
ShintuVarkey <shintu@citymapia.in>
2021-09-22 16:13:44 +0530

    migrate to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,56 @@
 const { guessProductionMode } = require("@ngneat/tailwind");
-
-process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
+const colors = require("tailwindcss/colors");
+process.env.TAILWIND_MODE = guessProductionMode() ? "build" : "watch";
 
 module.exports = {
-    prefix: '',
-    mode: 'jit',
-    purge: {
-      content: [
-        './src/**/*.{html,ts,css,scss,sass,less,styl}',
-      ]
+  prefix: "",
+  mode: "jit",
+  purge: {
+    content: ["./src/**/*.{html,ts,css,scss,sass,less,styl}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    colors: {
+      indigo: {
+        light: "#b3bcf5",
+        DEFAULT: "#5c6ac4",
+        dark: "#202e78",
+      },
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
     },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {},
+
+    spacing: {
+      13: "1.25rem",
+      15: "3.75rem",
+      128: "32rem",
+      144: "36rem",
     },
-    variants: {
-      extend: {},
+    extend: {
+      margin: {
+        "-72": "-18rem",
+      },
     },
-    plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/forms'),require('@tailwindcss/line-clamp'),require('@tailwindcss/typography')],
+  },
+  variants: {
+    extend: {},
+  },
+
+  // theme: {
+  //   colors: {
+  //     indigo: {
+  //       light: "#b3bcf5",
+  //       DEFAULT: "#5c6ac4",
+  //       dark: "#202e78",
+  //     },
+  //   },
+  // },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+  ],
 };
